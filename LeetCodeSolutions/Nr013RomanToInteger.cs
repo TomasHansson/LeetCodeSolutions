91db2475861f47bae75c1c96a7f386e5c2652733
Tomas Hansson <45672109+TomasHansson@users.noreply.github.com>
2019-01-03 15:45:21 +0100

    Add solutions to problems 12, 13 and 14.

---
+++ LeetCodeSolutions/Nr013RomanToInteger.cs
@@ -0,0 +1,54 @@
+﻿/*
+13. Roman To Integer
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+namespace LeetCodeSolutions
+{
+    class Nr013RomanToInteger
+    {
+        public int RomanToInt(string s)
+        {
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'I')
+                {
+                    if (i == s.Length - 1)
+                        result++;
+                    else if (s[i + 1] == 'X' || s[i + 1] == 'V')
+                        result--;
+                    else
+                        result++;
+                }
+                else if (s[i] == 'V')
+                    result += 5;
+                else if (s[i] == 'X')
+                {
+                    if (i == s.Length - 1)
+                        result += 10;
+                    else if (s[i + 1] == 'L' || s[i + 1] == 'C')
+                        result -= 10;
+                    else
+                        result += 10;
+                }
+                else if (s[i] == 'L')
+                    result += 50;
+                else if (s[i] == 'C')
+                {
+                    if (i == s.Length - 1)
+                        result += 100;
+                    else if (s[i + 1] == 'D' || s[i + 1] == 'M')
+                        result -= 100;
+                    else
+                        result += 100;
+                }
+                else if (s[i] == 'D')
+                    result += 500;
+                else if (s[i] == 'M')
+                    result += 1000;
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutions/Nr014LongestCommonPrefix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+14. Longest Common Prefix
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+*/
+
+namespace LeetCodeSolutions
+{
+    class Nr014LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+                return "";
+
+            Array.Sort(strs, (x, y) => x.Length.CompareTo(y.Length));
+            string result = "";
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                char testChar = strs[0][i];
+                bool allCommon = true;
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != testChar)
+                    {
+                        allCommon = false;
+                        break;
+                    }
+                }
+                if (allCommon)
+                    result += testChar;
+                else
+                    break;
+            }
+            return result;
+        }
+    }
+}
