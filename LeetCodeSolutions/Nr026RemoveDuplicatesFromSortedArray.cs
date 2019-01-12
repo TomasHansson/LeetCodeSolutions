95593b2269af563c1a6215f030f7d845c2644e46
Tomas Hansson <45672109+TomasHansson@users.noreply.github.com>
2019-01-12 19:35:44 +0100

    Add solutions to problems 26 and 27.

---
+++ LeetCodeSolutions/Nr026RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,29 @@
+﻿/* 
+26. Remove Duplicates From Sorted Array
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+*/
+
+namespace LeetCodeSolutions
+{
+    class Nr026RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+
+            if (nums.Length == 1)
+                return 1;
+
+            int uniqueNumbers = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                    nums[uniqueNumbers++] = nums[i];
+            }
+            return uniqueNumbers;
+        }
+    }
+}
