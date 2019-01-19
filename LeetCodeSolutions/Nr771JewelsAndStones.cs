2a1dbdea3f22ea5b59c8b508b975b33f349f6bc5
Tomas Hansson <45672109+TomasHansson@users.noreply.github.com>
2019-01-19 21:13:23 +0100

    Add solution to problem 771.

---
+++ LeetCodeSolutions/Nr771JewelsAndStones.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+/*
+771. Jewels and Stones
+
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have. 
+Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. 
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+*/
+
+namespace LeetCodeSolutions
+{
+    class Nr771JewelsAndStones
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            int numberOfJewels = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (J.Contains(S[i]))
+                    numberOfJewels++;
+            }
+            return numberOfJewels;
+        }
+    }
+}
